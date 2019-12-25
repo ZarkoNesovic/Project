3e4d067d11a8b43eed86eb736e5663192bd9de69
ZarkoNesovic <42474678+ZarkoNesovic@users.noreply.github.com>
2019-12-25 22:26:44 +0100

    25-Dec-2019

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.cookies);
+  console.log('------------');
+  console.log(req.session);
   res.render('index', { title: 'Express' });
 });
 
